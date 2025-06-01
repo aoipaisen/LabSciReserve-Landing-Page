2f47bd44ae47ce2ffc55b589719ea234c035a6f1
aoipaisen <bravo.kevinr@gmail.com>
2025-06-01 14:43:29 +0800

    First Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function signUp() {
+  alert("Redirecting to Sign Up Page...");
+  // window.location.href = "/signup.html";
+}
+
+function login() {
+  alert("Redirecting to Login Page...");
+  // window.location.href = "/login.html";
+}
+
+function reserveNow() {
+  alert("Starting reservation process...");
+  // window.location.href = "/reserve.html";
+}
